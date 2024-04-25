b840815e0104b8267179e762b010a1140b3c41bf
ajaxsuitest <muhammadarifproject@gmail.com>
2024-04-25 17:27:00 +0700

    Menambahkan List pada index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
     <h1>Hello world</h1> 
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
         Repellat ducimus omnis molestias, similique voluptate dolorem at adipisci voluptates error fugiat debitis. A cumque culpa maiores numquam facilis odit pariatur ex!</p>
-</body>
+
+        <ul>
+            <li>Nama  : Muhammad Arif </li>
+            <ul>Nim   : 21102058</ul>
+        </ul>
+    </body>
 </html>
