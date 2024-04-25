aefa2a25b1707f66eeda2fbceeaf373cbd8a390d
ajaxsuitest <muhammadarifproject@gmail.com>
2024-04-25 17:19:32 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,7 @@
 </head>
 <body>
     <h1>Hello world</h1> 
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Repellat ducimus omnis molestias, similique voluptate dolorem at adipisci voluptates error fugiat debitis. A cumque culpa maiores numquam facilis odit pariatur ex!</p>
 </body>
 </html>
